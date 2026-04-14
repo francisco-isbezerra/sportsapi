3affdd9d216e8f2e67c7559abf1f29c98d18738d
Francisco Iranildo <francisco.isb.dev@gmail.com>
2026-04-14 01:06:43 -0300

    Initial commit: Sports Ecommerce structure

---
+++ src/main/java/com/sportsapi/sports_ecommerce/SportsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.sportsapi.sports_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsEcommerceApplication.class, args);
+	}
+
+}
